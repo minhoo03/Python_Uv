10f84768d32e4f87cf383eb8fc1d46c78ac49a96
Gcy_03 <53499057+minhoo03@users.noreply.github.com>
2022-05-28 18:36:43 +0900

    Add files via upload

---
+++ code/ch07/7-10.py
@@ -0,0 +1,13 @@
+def mekeyprint(**kwargs):
+    for key in kwargs:
+        print('{}: {} '.format(key, kwargs[key]), end = '')
+    print()
+
+mekeyprint(여자친구=6, 마마무=4)
+mekeyprint(엑소=5, 트와이스=9, 블랙핑크=4, 방탄소년단=7)
+
+coffeeprice = {'에스프레소' :2500, '아메리카노':2800, '카페라떼':3200}
+mycar = {"brand":"현대", "model":"제네시스", "year":2016}
+
+mekeyprint(**coffeeprice)
+mekeyprint(**mycar)
